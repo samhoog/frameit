4b063e1187f95ee122d09c94fcd18f88b244aaf2
samhoog <samuel.hoogewind@icloud.com>
2022-12-05 19:00:28 -0500

    search bar updated to use api

--- frameit/src/app/app.module.ts
+++ frameit/src/app/app.module.ts
@@ -9,19 +9,25 @@ import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from 'src/components/home/home.component';
 import { GameComponent } from 'src/components/game/game.component';
+import { AboutComponent } from 'src/components/about/about.component';
+import { HelpsComponent } from 'src/components/helps/helps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GameComponent
+    GameComponent,
+    AboutComponent,
+    HelpsComponent
   ],
   imports: [
     BrowserModule,
@@ -32,9 +38,11 @@ import { GameComponent } from 'src/components/game/game.component';
     provideStorage(() => getStorage()),
     BrowserAnimationsModule,
     MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
     AngularFirestoreModule,
     AngularFireModule.initializeApp(environment.firebase),
-    FormsModule
+    FormsModule,
   ],
   providers: [AngularFirestore],
   bootstrap: [AppComponent]
--- frameit/src/components/game/game.component.ts
+++ frameit/src/components/game/game.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { AboutComponent } from '../about/about.component';
+import { HelpsComponent } from '../helps/helps.component';
 
 interface Movie { title: string, img1: string, img2: string, img3: string, img4: string, img5: string }
+interface Results { title: string }
+interface Movies { results: Results[] }
 
 @Component({
   selector: 'app-game',
@@ -18,11 +23,14 @@ export class GameComponent implements OnInit {
   wrongGuesses: string[] = [];
   currentImage = 1;
   currentSelectedImage = 1;
+  currentIndex = -1;
   searchedText = '';
   isCorrect = false;
   lost = false;
+  data = [];
+  allMovies: any[] = [];
 
-  constructor(public router: Router, private db: AngularFirestore) {
+  constructor(public router: Router, private db: AngularFirestore, private dialogRef: MatDialog) {
     // Save all the movie data in movieArray
     this.db.collection<Movie>('movies').valueChanges().subscribe((res => {
       if (res) {
@@ -34,7 +42,27 @@ export class GameComponent implements OnInit {
           this.movieTitles.push(movie.title);
         })
       }
+      this.fetchData();
     }))
+  }
+
+  async fetchData() {
+    // try getting the json data
+    let data = []
+    try {
+      const response = await fetch('https://imdb-api.com/en/API/Top250Movies/k_oarrley0');
+      const responseData = await response.json();
+      data = responseData?.items;
+      // if it failed, catch and print the error
+    } catch (err) {
+      console.error('Fetch Error :-S', err);
+    }
+    this.allMovies = data.map((movie: any) => movie['title']);
+    this.movieTitles.forEach(title => {
+      if (!this.allMovies.includes(title)) {
+        this.allMovies.push(title);
+      }
+    });
   }
 
   /**
@@ -44,7 +72,13 @@ export class GameComponent implements OnInit {
     // Choose a random movie from the array of movies
     this.currentImage = 1;
     this.currentSelectedImage = 1;
-    this.currentMovie = this.movieArray[Math.floor(Math.random() * this.movieArray.length)];
+    //this.currentMovie = this.movieArray[Math.floor(Math.random() * this.movieArray.length)];
+    if (this.currentIndex === this.movieArray.length - 1) {
+      this.currentMovie = this.movieArray[0];
+    } else {
+      this.currentIndex++;
+      this.currentMovie = this.movieArray[this.currentIndex];
+    }
     this.isCorrect = false;
     this.lost = false;
   }
@@ -60,6 +94,7 @@ export class GameComponent implements OnInit {
       this.searchedText = '';
       this.filteredMovies = [];
       this.wrongGuesses = [];
+      this.currentImage = 5;
     } else {
       this.isCorrect = false;
       this.wrongGuesses.push(this.searchedText);
@@ -89,7 +124,8 @@ export class GameComponent implements OnInit {
     if (this.searchedText === "") {
       this.filteredMovies = [];
     } else {
-      this.filteredMovies = this.movieTitles.filter((title) => title.toLowerCase().includes(this.searchedText.toLowerCase()));
+      //this.filteredMovies = this.movieTitles.filter((title) => title.toLowerCase().includes(this.searchedText.toLowerCase()));
+      this.filteredMovies = this.allMovies.filter((title) => title.toLowerCase().includes(this.searchedText.toLowerCase()));
     }
   }
 
@@ -101,7 +137,19 @@ export class GameComponent implements OnInit {
     this.currentSelectedImage = index;
   }
 
+  fillInMovie(clickedMovie: string): void {
+    this.searchedText = clickedMovie;
+    this.filteredMovies = [];
+  }
+
   ngOnInit(): void {
   }
 
+  openAbout(): void {
+    this.dialogRef.open(AboutComponent);
+  }
+
+  openHelp(): void {
+    this.dialogRef.open(HelpsComponent);
+  }
 }
